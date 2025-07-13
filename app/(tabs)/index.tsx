36a063217f83c385564e65ac3b25187f9ff66183
Andrei <andreistrauteugen@yahoo.com>
2025-07-14 01:44:12 +0300

    Implemented auth and tab nav

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -15,8 +15,12 @@ import "../globals.css";
 import * as Sentry from "@sentry/react-native";
 import { images, offers } from "@/constants";
 import { Background } from "@react-navigation/elements";
+import useAuthStore from "@/store/auth.store";
 
 export default function Index() {
+  const { user } = useAuthStore();
+
+  console.log("USER:", JSON.stringify(user, null, 2));
   return (
     <SafeAreaView className="flex-1 bg-white">
       <FlatList
@@ -84,14 +88,6 @@ export default function Index() {
               <CartButton />
             </View>
           </View>
-        )}
-        ListFooterComponent={() => (
-          <Button
-            title="Try!"
-            onPress={() => {
-              Sentry.captureException(new Error("First error"));
-            }}
-          />
         )}
       />
     </SafeAreaView>
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,10 +3,11 @@ import { useFonts } from "expo-font";
 import { useEffect } from "react";
 
 import "./globals.css";
-import * as Sentry from '@sentry/react-native';
+import * as Sentry from "@sentry/react-native";
+import useAuthStore from "@/store/auth.store";
 
 Sentry.init({
-  dsn: 'https://394ed3dac088ab30197a55d03f376a1c@o4509662368825344.ingest.de.sentry.io/4509662378393680',
+  dsn: "https://394ed3dac088ab30197a55d03f376a1c@o4509662368825344.ingest.de.sentry.io/4509662378393680",
 
   // Adds more context data to events (IP address, cookies, user, etc.)
   // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
@@ -15,13 +16,17 @@ Sentry.init({
   // Configure Session Replay
   replaysSessionSampleRate: 0.1,
   replaysOnErrorSampleRate: 1,
-  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+  integrations: [
+    Sentry.mobileReplayIntegration(),
+    Sentry.feedbackIntegration(),
+  ],
 
   // uncomment the line below to enable Spotlight (https://spotlightjs.com)
   // spotlight: __DEV__,
 });
 
 export default Sentry.wrap(function RootLayout() {
+  const { isLoading, fetchAuthenticatedUser } = useAuthStore();
   const [fontsLoaded, error] = useFonts({
     "QuickSand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
     "QuickSand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
@@ -34,6 +39,12 @@ export default Sentry.wrap(function RootLayout() {
     if (error) throw error;
     if (fontsLoaded) SplashScreen.hideAsync();
   }, [fontsLoaded, error]);
+
+  useEffect(() => {
+    fetchAuthenticatedUser();
+  }, []);
+
+  if (!fontsLoaded || isLoading) return null;
 
   return <Stack screenOptions={{ headerShown: false }} />;
 });
