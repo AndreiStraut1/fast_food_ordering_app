28f6edefa762715d26f43927d8778cd1fb2e4da2
Andrei <andreistrauteugen@yahoo.com>
2025-07-14 12:51:50 +0300

    Implemented search and filter

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -115,6 +115,7 @@ export const getCategories = async () => {
       appwriteConfig.databaseId,
       appwriteConfig.categoriesCollectionId
     );
+    return categories.documents;
   } catch (e) {
     throw new Error(e as string);
   }
